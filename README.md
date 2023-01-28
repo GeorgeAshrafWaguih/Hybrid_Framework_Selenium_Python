376a1d7cc4c2052e2eed9928f235a58220584e56
George <George.ashraf@hotmail.com>
2023-01-28 03:05:45 +0200

    adding configuration file

--- README.md
+++ README.md
@@ -8,6 +8,13 @@ Hybrid driven Framework with:
 * Openpyxl
 * Allure-pytest
 
+The framework has:
+* Login test case(test_login.py)
+* POM (LoginPage.py)
+* Ability to take screenshots in case of failure (Screenshot folder)
+* Ability to use common actions for test steps (conftest.py)
+* Common fields stored in configuration file(config.ini)
+* File to read the stored configuration in order to be used in tests (readProperties.py)
 
 AUT:
 * FrontEnd: https://demo.nopcommerce.com
--- test_Cases/test_login.py
+++ test_Cases/test_login.py
@@ -1,18 +1,19 @@
 import pytest
 from selenium import webdriver
 from pageObjects.LoginPage import Login
+from utilities.readProperties import ReadConfig
 
 
 class Test_001_Login:
-    baseURL = "https://admin-demo.nopcommerce.com/"
-    username = "admin@yourstore.com"
-    password = "admin"
+    baseURL = ReadConfig.getApplicationURL()
+    username = ReadConfig.getUseremail()
+    password = ReadConfig.getPassword()
 
     def test_homepagetitle(self, setup):
         self.driver = setup
         self.driver.get(self.baseURL)
         act_title = self.driver.title
-        if act_title == "Your store. Login1":
+        if act_title == "Your store. Login":
             self.driver.close()
             assert True
         else:
@@ -28,7 +29,7 @@ class Test_001_Login:
         self.lp.set_password(self.password)
         self.lp.click_login()
         act_title = self.driver.title
-        if act_title == "Dashboard / nopCommerce administration1":
+        if act_title == "Dashboard / nopCommerce administration":
             self.driver.close()
             assert True
         else:
