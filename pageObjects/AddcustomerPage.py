36ed425cfc62938b4a61ab7470e83af50d156b13
George <George.ashraf@hotmail.com>
2023-02-09 04:07:30 +0200

    Adding Add Customer Page objects

---
+++ pageObjects/AddcustomerPage.py
@@ -0,0 +1,98 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+
+class AddCustomer:
+    # Add customer page
+    lnkCustomers_menu_xpath = "/html/body/div[3]/aside/div/div[4]/div/div/nav/ul/li[4]/a/p"
+    lnkCustomers_menuitem_xpath = "/html/body/div[3]/aside/div/div[4]/div/div/nav/ul/li[4]/ul/li[1]/a/p"
+    btnAddnew_xpath = "/html/body/div[3]/div[1]/form[1]/div/div/a"
+    txtEmail_id = "Email"
+    txtPassword_id = "Password"
+    txtcustomerRole_xpath = '//*[@id="customer-info"]/div[2]/div[10]/div[2]/div/div[1]/div/div'
+    lstitemAdministrators_xpath = '//*[@id="SelectedCustomerRoleIds_listbox"]/li[1]'
+    lstitemRegistered_xpath = '//*[@id="SelectedCustomerRoleIds_listbox"]/li[4]'
+    lstitemGuests_xpath = '//*[@id="9eb4012a-6f1a-4f72-a691-e80ee592af10"]'
+    lstitemVendors_xpath = '//*[@id="SelectedCustomerRoleIds_listbox"]/li[5]'
+    drpmgrOfVendor_xpath = '//*[@id="VendorId"]'
+    rdMaleGender_id = "Gender_Male"
+    rdFeMaleGender_id = "Gender_Female"
+    txtFirstName_xpath = '//*[@id="FirstName"]'
+    txtLastName_xpath = '//*[@id="LastName"]'
+    txtDOB_xpath = '//*[@id="DateOfBirth"]'
+    txtCompanyName_xpath = '//*[@id="Company"]'
+    txtAdminContent_xpath = '//*[@id="AdminComment"]'
+    btnSave_xpath = '/html/body/div[3]/div[1]/form/div[1]/div/button[1]'
+
+    def __int__(self, driver):
+        self.driver = driver
+
+    def clickOnCustomerMenu(self):
+        self.driver.find_element(By.Xpath, self.lnkCustomers_menu_xpath).click()
+
+    def clickOnCustomerMenuItem(self):
+        self.driver.find_element(By.XPATH, self.lnkCustomers_menuitem_xpath).click()
+
+    def clickOnAddnew(self):
+        self.driver.find_element(By.XPATH, self.btnAddnew_xpath).click()
+
+    def setEmail(self, email):
+        self.driver.find_element(By.ID, self.txtEmail_id).send_keys(email)
+
+    def setPassword(self, password):
+        self.driver.find_element(By.ID, self.txtPassword_id).send_keys(password)
+
+    def setCustomerRoles(self, role):
+        self.driver.find_element(By.XPATH, self.txtcustomerRole_xpath).click()
+        time.sleep(3)
+        if role == 'Registered':
+            self.listitem = self.driver.find_element(By.XPATH, self.lstitemRegistered_xpath)
+        elif role == 'Administrators':
+            self.listitem = self.driver.find_element(By.XPATH, self.lstitemAdministrators_xpath)
+        elif role == 'Guests':
+            time.sleep(3)
+            self.driver.find_element(By.XPATH, "//*[@id='SelectedCustomerRoleIds_taglist']/li/span[2]").click()
+            self.listitem = self.driver.find_element(By.XPATH, self.lstitemGuests_xpath)
+        elif role == 'Registered':
+            self.listitem = self.driver.find_element(By.XPATH, self.lstitemRegistered_xpath)
+        elif role == 'Vendors':
+            self.listitem = self.driver.find_element(By.XPATH, self.lstitemVendors_xpath)
+        else:
+            self.listitem = self.driver.find_element(By.XPATH, self.lstitemGuests_xpath)
+        time.sleep(3)
+        self.driver.execute_script("arguments[0].click();", self.listitem)
+
+    def setManagerOfVendor(self, value):
+        drp = Select(self.driver.find_element(By.XPATH, self.drpmgrOfVendor_xpath))
+        drp.select_by_visible_text(value)
+
+    def setGender(self, gender):
+        if gender =='Male':
+            self.driver.find_element(By.XPATH, self.rdMaleGender_id).click()
+        elif gender =='Female':
+            self.driver.find_element(By.XPATH, self.rdFeMaleGender_id).click()
+        else:
+            self.driver.find_element(By.XPATH, self.rdMaleGender_id).click()
+
+    def setFirstName(self, fname):
+        self.driver.find_element(By.XPATH, self.txtFirstName_xpath).send_keys(fname)
+
+    def setLastName(self, lname):
+        self.driver.find_element(By.XPATH, self.txtLastName_xpath).send_keys(lname)
+
+    def setDob(self, dob):
+        self.driver.find_element(By.XPATH, self.txtDOB_xpath).send_keys(dob)
+
+    def setCompanyName(self, comname):
+        self.driver.find_element(By.XPATH, self.txtCompanyName_xpath).send_keys(comname)
+
+    def setAdminContent(self, content):
+        self.driver.find_element(By.XPATH, self.txtAdminContent_xpath).send_keys(content)
+
+    def clickOnSave(self):
+        self.driver.find_element(By.XPATH, self.btnSave_xpath).click()
+
+
+
