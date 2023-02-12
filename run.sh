42a5fedc12d93fdc5027660496e44c356f76a6b9
George <George.ashraf@hotmail.com>
2023-02-12 19:02:46 +0200

    adding sh file

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ REM pytest -s -v -m "regression" --html=Reports/report.html --browser chrome
 REM pytest -s -v -m "regression and sanity" --html=Reports/report.html --browser chrome
 REM pytest -s -v -m "regression or sanity" --html=Reports/report.html --browser chrome
 
-REM firefox
+REM firefox 
 pytest -s -v -m "sanity" --html=Reports/report.html --browser firefox
 REM pytest -s -v -m "regression" --html=Reports/report.html --browser firefox
 REM pytest -s -v -m "regression and sanity" --html=Reports/report.html --browser firefox
